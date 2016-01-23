39e84effbe433d116748e098af0cec6710857d99
jiajidi <jiajidi@163.com>
2016-01-23 11:52:44 +0800

    增加了函数类

---
+++ Lyyneheym/LyyneheymCore/SlyviaCore/SceneFunction.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LyyneheymCore.SlyviaCore
+{
+    /// <summary>
+    /// 函数调用类：处理场景里的函数
+    /// </summary>
+    public class SceneFunction
+    {
+        /// <summary>
+        /// 构造器
+        /// </summary>
+        public SceneFunction(string callname, string parent, SceneAction sa = null)
+        {
+            this.parentSceneName = parent;
+            this.callname = callname;
+            this.sa = sa;
+        }
+
+        // 在变量字典中的名字
+        public string varDictName
+        {
+            get
+            {
+                return String.Format("___SlyviaFunc_{0}?{1}", this.callname, this.parentSceneName);
+            }
+            private set;
+        }
+        // 绑定动作序列
+        public SceneAction sa = null;
+        // 函数名
+        public string callname = null;
+        // 场景名称
+        public string parentSceneName = null;
+    }
+}
