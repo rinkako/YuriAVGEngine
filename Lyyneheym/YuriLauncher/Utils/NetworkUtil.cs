f4ddf64f92e46abafc8d06c061f84a7a335cd3ae
rinkako <jiajidi@163.com>
2017-03-25 02:48:15 +0800

    Add Launcher

---
+++ Lyyneheym/YuriLauncher/Utils/NetworkUtil.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuri.YuriLauncher.Utils
+{
+    /// <summary>
+    /// 为程序提供网络服务
+    /// </summary>
+    internal static class NetworkUtil
+    {
+
+    }
+}
