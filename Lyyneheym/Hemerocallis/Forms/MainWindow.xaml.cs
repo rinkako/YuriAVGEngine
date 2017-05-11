a42c58f4109f8445e9c74866456089b0c9c623a9
Rinkako <jiajidi@163.com>
2017-05-12 01:34:19 +0800

    Hemo update

--- Lyyneheym/Hemerocallis/Forms/MainWindow.xaml.cs
+++ Lyyneheym/Hemerocallis/Forms/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using MahApps.Metro.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using MahApps.Metro.Controls;
 
 namespace Yuri.Hemerocallis.Forms
 {
@@ -10,6 +13,33 @@ namespace Yuri.Hemerocallis.Forms
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        Point pos = new Point();
+
+        private void Canvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            var tmp = (Canvas)sender;
+            pos = e.GetPosition(null);
+            tmp.CaptureMouse();
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                var tmp = (Canvas)sender;
+                double dx = e.GetPosition(null).X - pos.X + tmp.Margin.Left;
+                double dy = e.GetPosition(null).Y - pos.Y + tmp.Margin.Top;
+                tmp.Margin = new Thickness(dx, dy, 0, 0);
+                pos = e.GetPosition(null);
+            }
+        }
+
+        private void Canvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var tmp = (Canvas)sender;
+            tmp.ReleaseMouseCapture();
         }
     }
 }
