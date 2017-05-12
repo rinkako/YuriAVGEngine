3bcfcd5a30d567000191fa876b43003202added9
Rinkako <jiajidi@163.com>
2017-05-12 18:01:50 +0800

    Hemerocallis UI

---
+++ Lyyneheym/Hemerocallis/Forms/AboutWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Yuri.Hemerocallis.Forms
+{
+    /// <summary>
+    /// AboutWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
+        }
+    }
+}
--- Lyyneheym/Hemerocallis/Forms/MainWindow.xaml.cs
+++ Lyyneheym/Hemerocallis/Forms/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Input;
 using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace Yuri.Hemerocallis.Forms
 {
@@ -13,6 +14,29 @@ namespace Yuri.Hemerocallis.Forms
         public MainWindow()
         {
             InitializeComponent();
+            var item = new TreeViewItem()
+            {
+                Header = "萱草，忘却的爱"
+            };
+            int t = this.TreeView_ProjectTree.Items.Add(item);
+            var root = item;
+            item = new TreeViewItem()
+            {
+                Header = "Chapter 1 - 于盛夏之末"
+            };
+            root.Items.Add(item);
+            var pp = item = new TreeViewItem()
+            {
+                Header = "Chapter 2 - 恋恋不舍"
+            };
+            root.Items.Add(item);
+            item = new TreeViewItem()
+            {
+                Header = "Chapter 2.1 - 我是第二级"
+            };
+            pp.Items.Add(item);
+            (this.TreeView_ProjectTree.Items[0] as TreeViewItem).IsExpanded = true;
+            
         }
 
         Point pos = new Point();
@@ -29,9 +53,10 @@ namespace Yuri.Hemerocallis.Forms
             if (e.LeftButton == MouseButtonState.Pressed)
             {
                 var tmp = (Canvas)sender;
-                double dx = e.GetPosition(null).X - pos.X + tmp.Margin.Left;
-                double dy = e.GetPosition(null).Y - pos.Y + tmp.Margin.Top;
-                tmp.Margin = new Thickness(dx, dy, 0, 0);
+                double dx = e.GetPosition(null).X - pos.X + Canvas.GetLeft(tmp);
+                double dy = e.GetPosition(null).Y - pos.Y + Canvas.GetTop(tmp);
+                Canvas.SetLeft(tmp, dx);
+                Canvas.SetTop(tmp, dy);
                 pos = e.GetPosition(null);
             }
         }
@@ -40,6 +65,33 @@ namespace Yuri.Hemerocallis.Forms
         {
             var tmp = (Canvas)sender;
             tmp.ReleaseMouseCapture();
+        }
+        
+
+        private void Button_Click_TitleTreeToggle(object sender, RoutedEventArgs e)
+        {
+            if (this.isProjectTreeVisible)
+            {
+                this.MainGrid_Col_0.MaxWidth = this.MainGrid_Col_0.MinWidth = 0;
+            }
+            else
+            {
+                this.MainGrid_Col_0.MaxWidth = this.MainGrid_Col_0.MinWidth = 256;
+            }
+            this.isProjectTreeVisible = !this.isProjectTreeVisible;
+        }
+        private bool isProjectTreeVisible = true;
+
+        private void Image_MouseLeftButtonUp_MenuBtn(object sender, MouseButtonEventArgs e)
+        {
+            this.Flyout_Menu.IsOpen = true;
+        }
+
+        private void Button_Click_Menu_About(object sender, RoutedEventArgs e)
+        {
+            this.Flyout_Menu.IsOpen = false;
+            AboutWindow aw = new AboutWindow();
+            aw.ShowDialog();
         }
     }
 }
