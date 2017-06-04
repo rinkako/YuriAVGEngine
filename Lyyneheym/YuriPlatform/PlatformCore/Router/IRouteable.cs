79b3d6d78b94320e156e1d9b7f4152a66d7b0056
Rinkako <jiajidi@163.com>
2017-06-04 23:14:22 +0800

    router prepare

---
+++ Lyyneheym/YuriPlatform/PlatformCore/Router/IRouteable.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yuri.PlatformCore.Router
+{
+    /// <summary>
+    /// 可消息路由项目
+    /// </summary>
+    public interface IRouteable
+    {
+        /// <summary>
+        /// 冒泡触发
+        /// </summary>
+        void TriggerBubbling(YuriRoutedEvent evt);
+
+        /// <summary>
+        /// 隧道触发
+        /// </summary>
+        void TriggerTunneling(YuriRoutedEvent evt);
+
+        /// <summary>
+        /// 直接触发
+        /// </summary>
+        void TriggersDirect(YuriRoutedEvent evt);
+
+        /// <summary>
+        /// 获取或设置路由的唯一标识符名字
+        /// </summary>
+        string Name { get; set; }
+
+        /// <summary>
+        /// 获取或设置路由是否吞下经过的事件
+        /// </summary>
+        bool IsSwallow { get; set; }
+
+        /// <summary>
+        /// 获取或设置路由的上级路由
+        /// </summary>
+        IRouteable Parent { get; set; }
+
+        /// <summary>
+        /// 获取或设置路由的子级路由
+        /// </summary>
+        List<IRouteable> Children { get; set; }
+    }
+
+    /// <summary>
+    /// 枚举：路由类型
+    /// </summary>
+    public enum YuriRoutedType
+    {
+        /// <summary>
+        /// 直接
+        /// </summary>
+        Direct,
+        /// <summary>
+        /// 冒泡：自底向上
+        /// </summary>
+        Bubbling,
+        /// <summary>
+        /// 隧道：自顶向下
+        /// </summary>
+        Tunneling
+    }
+
+}
---
+++ Lyyneheym/YuriPlatform/PlatformCore/Router/YuriRoutedEvent.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Yuri.PlatformCore.Router
+{
+    /// <summary>
+    /// 路由事件类：为引擎模块间通讯提供消息包装
+    /// </summary>
+    public class YuriRoutedEvent
+    {
+        /// <summary>
+        /// 获取或设置事件的路由类型
+        /// </summary>
+        public YuriRoutedType Type { get; set; }
+
+        /// <summary>
+        /// 获取或设置事件的名字
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 获取或设置事件的唯一标识符
+        /// </summary>
+        public string Id { get; set; }
+        
+        /// <summary>
+        /// 获取或设置事件的附加值
+        /// </summary>
+        public object Tag { get; set; }
+
+        /// <summary>
+        /// 获取或设置事件发出路由器的名字
+        /// </summary>
+        public string FromRouter { get; set; }
+
+        /// <summary>
+        /// 获取或设置事件最终目的地路由器的名字
+        /// </summary>
+        public string ToRouter { get; set; }
+
+        /// <summary>
+        /// 获取或设置上一路由器的名字
+        /// </summary>
+        public string LastRouter { get; set; }
+        
+        /// <summary>
+        /// 获取或设置事件是否已经被处理
+        /// </summary>
+        public bool IsHandled { get; set; } = false;
+
+        /// <summary>
+        /// 获取或设置事件是否已经被取消
+        /// </summary>
+        public bool IsCanceled { get; set; } = false;
+    }
+}
